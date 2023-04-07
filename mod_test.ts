813f0f2c27827d4d5e61a9ce5bb5ec1cae91d232
himanoa <matsunoappy@gmail.com>
2023-04-08 06:05:29 +0900

    Consolidated helper functions for creating validators into makeValidator function

--- mod_test.ts
+++ mod_test.ts
@@ -1,31 +1,29 @@
 import { assertEquals } from "https://deno.land/std@0.181.0/testing/asserts.ts";
-import { Err, makeValidationSmartConstructor, Ok, Validator } from "./mod.ts";
+import { makeValidationSmartConstructor, makeValidator } from "./mod.ts";
 
 declare const UserSym: unique symbol;
 
 Deno.test("It return Ok value when ValidationSmartConstructor is received valid value", () => {
-  const validValidator: Validator<string, { kind: "notNumber" }> = (
-    v: string,
-  ) => {
-    return Ok(v);
-  };
+  const alwaysValidValidator = makeValidator<string, unknown>((ok) => (value) =>
+    ok(value)
+  );
   const userSmartConstructor = makeValidationSmartConstructor<
     string,
     typeof UserSym
-  >()(validValidator);
+  >()(alwaysValidValidator);
 
   assertEquals(userSmartConstructor("xxx").v, "xxx");
 });
 
 Deno.test("It return Err value when ValidationSmartConstructor is received invalid value", () => {
-  const numberValidator: Validator<string, { kind: "notNumber" }> = (
-    v: string,
-  ) => {
-    if (isNaN(parseInt(v, 10))) {
-      return Err({ "kind": "notNumber" });
-    }
-    return Ok(v);
-  };
+  const numberValidator = makeValidator<string, { kind: "notNumber" }>(
+    (ok, err) => (value) => {
+      if (isNaN(parseInt(value, 10))) {
+        return err({ "kind": "notNumber" });
+      }
+      return ok(value);
+    },
+  );
   const userSmartConstructor = makeValidationSmartConstructor<
     string,
     typeof UserSym
