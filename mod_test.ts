3007d69a27f868161027d96b9e9f38febf817e59
himanoa <matsunoappy@gmail.com>
2023-04-17 06:34:59 +0900

    Fix test case

--- mod_test.ts
+++ mod_test.ts
@@ -1,5 +1,9 @@
 import { assertEquals } from "https://deno.land/std@0.181.0/testing/asserts.ts";
-import { Validator, makeValidationSmartConstructor, makeValidator } from "./mod.ts";
+import {
+  makeValidationSmartConstructor,
+  makeValidator,
+  Validator,
+} from "./mod.ts";
 
 declare const UserSym: unique symbol;
 
@@ -16,19 +20,20 @@ Deno.test("It return Ok value when ValidationSmartConstructor is received valid 
 });
 
 Deno.test("It return Err value when ValidationSmartConstructor is received invalid value", () => {
-  const urlVaidator: Validator<string, { kind: "invalidFormat" }> = makeValidator<string, { kind: 'invalidFormat' }>((ok, err) => (value) => {
-    try {
-      new URL(value)
-      return ok(value)
-    } catch {
-      return err({kind: 'invalidFormat'})
-    }
-  })
+  const urlVaidator: Validator<string, { kind: "invalidFormat" }> =
+    makeValidator<string, { kind: "invalidFormat" }>((ok, err) => (value) => {
+      try {
+        new URL(value);
+        return ok(value);
+      } catch {
+        return err({ kind: "invalidFormat" });
+      }
+    });
 
   const userSmartConstructor = makeValidationSmartConstructor<
     string,
     typeof UserSym
   >()(urlVaidator);
 
-  assertEquals(userSmartConstructor("xxx").e, { kind: "notNumber" });
+  assertEquals(userSmartConstructor("xxx").e, { kind: "invalidFormat" });
 });
